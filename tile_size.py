34977947e3933ef7d85361c869496d148bafb891
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-23 16:13:30 -0500

    Remove cruft

--- tile_size.py
+++ tile_size.py
@@ -9,7 +9,6 @@ c.execute('SELECT zoom_level,tile_row,tile_column,tile_id FROM map')
 tiles = c.fetchall()
 print len(tiles)
 size = [[]]*(14+1)
-co=0
 for tile in tiles:
 	tile_id = tile[3]
 	zoom = tile[0]
