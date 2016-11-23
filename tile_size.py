ced1caafda7a9b37947d02260b34fab4f818b3fc
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-23 14:57:42 -0500

    Upload tile_size.py

---
+++ tile_size.py
@@ -0,0 +1,21 @@
+import sqlite3
+import sys
+import matplotlib.pyplot as plt
+
+file = sys.argv[1]
+conn = sqlite3.connect(file)
+c = conn.cursor()
+c.execute('SELECT zoom_level,tile_row,tile_column,tile_id FROM map')
+tiles = c.fetchall()
+print len(tiles)
+size = [[]]*(14+1)
+co=0
+for tile in tiles:
+	tile_id = tile[3]
+	zoom = tile[0]
+	c.execute('SELECT tile_data FROM images WHERE tile_id = "'+tile_id+'"')
+	tile_size = len(c.fetchall()[0][0])
+	size[zoom]=size[zoom]+[tile_size]
+
+plt.boxplot(size)
+plt.show()
