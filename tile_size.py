6725a0a445d8301d33b7a9fcfaf540d495a84cdb
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-23 19:35:29 -0500

    major performance improvement, proper labels

--- tile_size.py
+++ tile_size.py
@@ -1,20 +1,23 @@
 import sqlite3
 import sys
+import itertools
 import matplotlib.pyplot as plt
 
 file = sys.argv[1]
+
 conn = sqlite3.connect(file)
 c = conn.cursor()
-c.execute('SELECT zoom_level,tile_row,tile_column,tile_id FROM map')
-tiles = c.fetchall()
-print len(tiles)
-size = [[]]*(14+1)
-for tile in tiles:
-	tile_id = tile[3]
-	zoom = tile[0]
-	c.execute('SELECT tile_data FROM images WHERE tile_id = "'+tile_id+'"')
-	tile_size = len(c.fetchall()[0][0])
-	size[zoom]=size[zoom]+[tile_size]
+
+max_zoom = 14
+size = [[]]*(max_zoom+1)
+for zoom in xrange(0,max_zoom+1):
+	c.execute('SELECT tile_data FROM map INNER JOIN images on map.tile_id = images.tile_id AND map.zoom_level = "'+str(zoom)+'"')  # Get tiles based on tile_id matching in inner join, that way the zoom_level can be accessed from the images tables
+	tiles=list(itertools.chain.from_iterable(list(c)))	# Flatten results to a single list
+	size[zoom]=[len(tile) for tile in tiles]		# Get the size
 
 plt.boxplot(size)
+plt.title('Tile size')
+plt.xlabel('Zoom')
+plt.ylabel('Size (bytes)')
+plt.xticks(xrange(1,max_zoom+2), xrange(0,max_zoom+1))
 plt.show()
