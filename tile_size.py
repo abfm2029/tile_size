9238e7a8f125069d20ef76a122be4809b668d8c9
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-24 14:43:57 -0500

    performance increase, compatible with python 3

--- tile_size.py
+++ tile_size.py
@@ -9,15 +9,15 @@ conn = sqlite3.connect(file)
 c = conn.cursor()
 
 max_zoom = 14
+
 size = [[]]*(max_zoom+1)
-for zoom in xrange(0,max_zoom+1):
-	c.execute('SELECT tile_data FROM map INNER JOIN images on map.tile_id = images.tile_id AND map.zoom_level = "'+str(zoom)+'"')  # Get tiles based on tile_id matching in inner join, that way the zoom_level can be accessed from the images tables
-	tiles=list(itertools.chain.from_iterable(list(c)))	# Flatten results to a single list
-	size[zoom]=[len(tile) for tile in tiles]		# Get the size
+for zoom in range(0,max_zoom+1):
+        c.execute('SELECT length(tile_data) FROM map INNER JOIN images on map.tile_id = images.tile_id AND map.zoom_level = "'+str(zoom)+'"')  # Get tiles based on tile_id matching in inner join, that way the zoom_level can be accessed $
+        size[zoom] = list(itertools.chain.from_iterable(list(c)))
 
 plt.boxplot(size)
 plt.title('Tile size')
 plt.xlabel('Zoom')
 plt.ylabel('Size (bytes)')
-plt.xticks(xrange(1,max_zoom+2), xrange(0,max_zoom+1))
+plt.xticks(range(1,max_zoom+2), range(0,max_zoom+1))
 plt.show()
