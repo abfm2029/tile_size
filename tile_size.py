6b0a707b0aa72041990e230f4d464b2475991ce1
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-27 13:45:13 -0500

    Fix caps

--- tile_size.py
+++ tile_size.py
@@ -13,7 +13,7 @@ max_zoom = list(c)[0][0]
 
 size = [[]]*(max_zoom+1)
 for zoom in range(0,max_zoom+1):
-	c.execute('SELECT length(tile_data) FROM tiles INNER JOIN logs where logs.zoom = "'+str(zoom)+'" AND tiles.tile_column = logs.x AND tiles.tile_row = logs.y AND tiles.zoom_level = logs.zoom;')
+	c.execute('SELECT length(tile_data) FROM tiles INNER JOIN logs WHERE logs.zoom = "'+str(zoom)+'" AND tiles.tile_column = logs.x AND tiles.tile_row = logs.y AND tiles.zoom_level = logs.zoom;')
 	size[zoom] = list(itertools.chain.from_iterable(list(c)))
 
 plt.boxplot(size)
