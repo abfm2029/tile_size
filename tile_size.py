56792744bd0b57eb124ac4230acf195e0b058513
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-30 19:03:01 -0500

    Hard code top decile

--- tile_size.py
+++ tile_size.py
@@ -1,5 +1,6 @@
 import sqlite3
 import sys
+import math
 import itertools
 import matplotlib.pyplot as plt
 
@@ -11,10 +12,13 @@ c = conn.cursor()
 c.execute('SELECT MAX(zoom_level) FROM map')
 max_zoom = list(c)[0][0]
 
-size = [[]]*(max_zoom+1)
+size = [None]*(max_zoom+1)
+top_decile = [None]*(max_zoom+1)
 for zoom in range(0,max_zoom+1):
 	c.execute('SELECT length(tile_data) FROM map INNER JOIN images on map.tile_id = images.tile_id AND map.zoom_level = "'+str(zoom)+'"')  # Get tiles based on tile_id matching in inner join, that way the zoom_level can be accessed from the images tables
-	size[zoom] = list(itertools.chain.from_iterable(list(c)))
+	size[zoom] = sorted(list(itertools.chain.from_iterable(list(c))))
+	leng = len(size[zoom])
+	top_decile[zoom] = size[zoom][int(math.floor(leng*0.9)):]
 
 plt.boxplot(size)
 plt.title('Tile size')
@@ -22,3 +26,11 @@ plt.xlabel('Zoom')
 plt.ylabel('Size (bytes)')
 plt.xticks(range(1,max_zoom+2), range(0,max_zoom+1))
 plt.show()
+
+plt.boxplot(top_decile)
+plt.title('Tile size')
+plt.xlabel('Zoom')
+plt.ylabel('Size (bytes)')
+plt.xticks(range(1,max_zoom+2), range(0,max_zoom+1))
+plt.show()
+
